ba89430776ecbebfafe1c426afe61f1bf78f7282
Patrick Young <patrickyoung281@gmail.com>
2022-11-01 15:38:48 -0400

    commit tree b8e9376556323345c5ff159a103ef7f204370a3a

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,18 @@
 class Student < ActiveRecord::Base
 end
+
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "db/students.sqlite"
+)
+
+sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY
+    )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
+  
+  
+
